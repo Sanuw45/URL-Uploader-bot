2ec1a12472f7b0d2609f6ccc984e0c55faafb448
Sanuw45 <92355575+Sanuw45@users.noreply.github.com>
2022-01-25 06:34:33 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,5 +1,5 @@
 class Translation(object):
-    START_TEXT = """Hi {},
+    START_TEXT = """Hi  {},
 I'm X-URL Uploader!
 You can upload HTTP/HTTPS direct link, Using this bot!
 
@@ -7,8 +7,8 @@ You can upload HTTP/HTTPS direct link, Using this bot!
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
-    DOWNLOAD_START = "Now Downloading.."
-    UPLOAD_START = "Now Uploading.."
+    DOWNLOAD_START = "ඔයා දුන්න එක download වෙනව හලෝ "
+    UPLOAD_START = "ඔන්න ඒක ඔයාටඑවනවා "
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 2GB due to Telegram API limitations."
     AFTER_SUCCESSFUL_UPLOAD_MSG = "Thanks for using @xurluploaderbot)"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds.\n\n@xurluploaderbot"
